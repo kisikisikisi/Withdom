05a95c4c9046504fa2fe33a6548b9f79fb8ca714
kisikisi <ki.shu1125bi@gmail.com>
2021-01-03 19:14:11 +0900

    markdownエディターの追加

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from mdeditor.fields import MDTextField # 追加
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     published = models.DateTimeField()
     image = models.ImageField(upload_to='media/')
-    body = models.TextField()
+    body = MDTextField()
 
     def __str__(self):
         return self.title
