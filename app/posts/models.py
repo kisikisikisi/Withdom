755fbfe45113c7bde2921a1e95394d9ccf526348
kisikisi <ki.shu1125bi@gmail.com>
2021-01-12 12:54:08 +0900

    デフォルトのタイムスタンプを設定

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from mdeditor.fields import MDTextField # 追加
+from mdeditor.fields import MDTextField
+from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    published = models.DateTimeField()
+    published = models.DateTimeField(default=timezone.now)
     image = models.ImageField(upload_to='media/')
     body = MDTextField()
 
