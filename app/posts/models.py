281a3ffd281a822e02835f894c0eb57d6c1de8fd
kisikisi <ki.shu1125bi@gmail.com>
2021-01-16 17:33:19 +0900

    投稿ページのレイアウト更新

--- app/posts/models.py
+++ app/posts/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    published = models.DateTimeField(default=timezone.now)
+    published = models.DateTimeField(default=timezone.now, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     image = models.ImageField(upload_to='media/', blank=True)
     body = MDTextField()
