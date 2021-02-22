fd120f78a9700915fa132be30c14242d762e093e
kisikisi <ki.shu1125bi@gmail.com>
2021-02-22 20:33:55 +0900

    投稿数取得用urlの追加

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -7,6 +7,7 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('get_posts_count/<str:username>/', views.get_posts_count, name='get_posts_count'),
     path('posts/<int:post_id>/', views.post_detail, name="post_detail"),
     path('add/', views.add, name='add'),
     path('edit/<int:post_id>/', views.edit, name='edit'),
